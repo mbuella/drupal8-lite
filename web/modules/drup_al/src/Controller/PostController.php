3b50cf57282e502ff1aa6d8c8a73e7eb8948eaa1
Marlon Buella <marlon.buella@movent.com>
2017-11-21 12:30:59 +0800

    Update post list feature

--- web/modules/drup_al/src/Controller/PostController.php
+++ web/modules/drup_al/src/Controller/PostController.php
@@ -7,9 +7,33 @@ namespace Drupal\drup_al\Controller;
 
 class PostController {
   public function listPost() {
+    $postList = [];
+    
+    //fetch ids (nids), with conditions (type of post)
+    $query = \Drupal::entityQuery('node')
+                      ->condition('type', 'post');
+    $nids = $query->execute();
+    
+    // fetch nodes using the ids
+    $node_storage = \Drupal::entityManager()->getStorage('node'); 
+    $mult = $node_storage->loadMultiple($nids);
+    
+    
+    // loop through the fetch nodes
+    foreach($mult as $entity) {
+      $postList[] = array(
+        'title' => $entity->get('title')->value,
+        'intro' => $entity->get('body')->summary,
+        // title value of the category entity reference (entity field)
+        'category' => $entity->get('field_post_category')->entity->get('title')->value,
+      );
+    }
+    
+    // dump($postList);    
+    
     return array(
       '#theme' => 'post_list',
-      '#posts' => t('Hello, World!'),
+      '#posts' => $postList,
     );
   }
 }
