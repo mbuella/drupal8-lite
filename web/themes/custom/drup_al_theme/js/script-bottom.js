08bdf70ace0de7babb5037254d51535942599539
Marlon Buella <marlon.buella@movent.com>
2017-12-15 20:03:56 +0800

    Build Drupal 8 lite theme

---
+++ web/themes/custom/drup_al_theme/js/script-bottom.js
@@ -0,0 +1,15 @@
+(function ($, Drupal) {
+  "use strict";
+  
+  Drupal.behaviors.basic = {
+    attach: function attach(context) {
+      
+      /** Start JS/jQuery here **/
+      console.log('Welcome to Drupal 8 Lite!');
+          
+      /** End of JS/jQuery here **/
+      
+    }
+  };
+  
+})(jQuery, Drupal);	
